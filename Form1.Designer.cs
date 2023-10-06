9fe2c85fc7b55ad527cd86cdb99f988637a4b3d8
Alena Samus <elensawelu@gmail.com>
2023-10-06 14:56:04 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace ExcelImportExport
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ImportBtn = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.ResultsList = new System.Windows.Forms.ListBox();
+            this.ResultsList2 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // ImportBtn
+            // 
+            this.ImportBtn.Cursor = System.Windows.Forms.Cursors.Cross;
+            this.ImportBtn.Location = new System.Drawing.Point(41, 32);
+            this.ImportBtn.Name = "ImportBtn";
+            this.ImportBtn.Size = new System.Drawing.Size(91, 34);
+            this.ImportBtn.TabIndex = 0;
+            this.ImportBtn.Text = "Import From Excel";
+            this.ImportBtn.UseVisualStyleBackColor = true;
+            this.ImportBtn.Click += new System.EventHandler(this.ImportBtn_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // ResultsList
+            // 
+            this.ResultsList.FormattingEnabled = true;
+            this.ResultsList.Location = new System.Drawing.Point(210, 32);
+            this.ResultsList.Name = "ResultsList";
+            this.ResultsList.Size = new System.Drawing.Size(264, 173);
+            this.ResultsList.TabIndex = 1;
+            // 
+            // ResultsList2
+            // 
+            this.ResultsList2.FormattingEnabled = true;
+            this.ResultsList2.Location = new System.Drawing.Point(493, 32);
+            this.ResultsList2.Name = "ResultsList2";
+            this.ResultsList2.Size = new System.Drawing.Size(264, 173);
+            this.ResultsList2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.ResultsList2);
+            this.Controls.Add(this.ResultsList);
+            this.Controls.Add(this.ImportBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ImportBtn;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ListBox ResultsList;
+        private ListBox ResultsList2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace ExcelImportExport
+{
+    public partial class Form1 : Form
+    {
+        string[,] list = new string[50, 5];
+
+        List<List<string>> list_ = new List<List<string>>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ImportBtn_Click(object sender, EventArgs e)
+        {
+            int n = ExportExcel();
+
+            ResultsList.Items.Clear();
+            
+            string s;
+
+            foreach(var item in list_)
+            {
+                s = "";
+                foreach(var elem in item)
+                {
+                    s +="|" + elem.ToString();
+                }
+
+                ResultsList2.Items.Add(s);
+            }
+
+            for (int i = 0; i < n; i++) // по всем строкам
+            {
+                s = "";
+                for (int j = 0; j < 5; j++) //по всем колонкам
+                    s += " | " + list[i, j];
+                ResultsList.Items.Add(s);
+            }
+
+        }
+        // Импорт данных из Excel-файла (не более 5 столбцов и любое количество строк <= 50.
+        private int ExportExcel()
+        {
+            List <string> temp = new List<string>();
+            // Выбрать путь и имя файла в диалоговом окне
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            // Задаем расширение имени файла по умолчанию (открывается папка с программой)
+            ofd.DefaultExt = "*.xls;*.xlsx";
+
+            // Задаем строку фильтра имен файлов, которая определяет варианты
+            ofd.Filter = "файл Excel (Spisok.xlsx)|*.xlsx";
+
+            // Задаем заголовок диалогового окна
+            ofd.Title = "Выберите файл базы данных";
+
+            if (!(ofd.ShowDialog() == DialogResult.OK)) // если файл БД не выбран -> Выход
+                return 0;
+
+            Excel.Application ObjWorkExcel = new Excel.Application();
+
+            Excel.Workbook ObjWorkBook = ObjWorkExcel.Workbooks.Open(ofd.FileName);
+
+            Excel.Worksheet ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1]; //получить 1-й лист
+
+            var lastCell = ObjWorkSheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell);//последнюю ячейку
+                                                                                                // размеры базы
+            int lastColumn = (int)lastCell.Column;
+
+            int lastRow = (int)lastCell.Row;
+
+            // Перенос в промежуточный массив класса Form1: string[,] list = new string[50, 5]; 
+            for (int j = 0; j < lastColumn; j++) //по всем колонкам
+            {
+                for (int i = 0; i < lastRow; i++) // по всем строкам
+                {
+                    temp.Add("|" + ObjWorkSheet.Cells[i + 1, j + 1].Text.ToString());
+
+                    list[i, j] = ObjWorkSheet.Cells[i + 1, j + 1].Text.ToString(); //считываем данные
+                }
+
+                list_.Add(temp);
+            }
+            ObjWorkBook.Close(false, Type.Missing, Type.Missing); //закрыть не сохраняя
+            
+            ObjWorkExcel.Quit(); // выйти из Excel
+            
+            GC.Collect(); // убрать за собой
+            
+            return lastRow;
+        }
+    }
+}
